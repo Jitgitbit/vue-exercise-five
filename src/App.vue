b8de860a1c8231c5462ff9730bd86aab549a0295
Jitgitbit <rowowolf@gmail.com>
2020-05-03 14:17:30 +0200

    improved colors

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,26 @@ export default {
 </script>
 
 <style>
-/* #app {
+#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  background-color: #2c3e50;
   margin-top: 60px;
-} */
+}
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 0;
+  background-size: 100%;
+  background-position: bottom;
+  background-color: teal;
+  min-height: 100%;
+}
 </style>
